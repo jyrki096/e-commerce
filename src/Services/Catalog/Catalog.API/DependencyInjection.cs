92d2cd8867c5be6e4071e78197404a6ed44673eb
Юрий Гладких <jyrki096@gmail.com>
2025-12-11 08:15:48 +0300

    BrandsController для получения брендов через MediatR

--- src/Services/Catalog/Catalog.API/DependencyInjection.cs
+++ src/Services/Catalog/Catalog.API/DependencyInjection.cs
@@ -4,13 +4,21 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddControllers();
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+        
+        var assembly = typeof(GetBrandsQuery).Assembly;
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(assembly);
+        });
         return services;
     }
 
     public static WebApplication UseApiServices(this WebApplication app)
     {
+        app.MapControllers();
         app.UseSwagger();
         app.UseSwaggerUI();
         return app;
